849715d3cc0edd48e71d8108de3704e8bc40a6ff
argajienbi <armin.gandi@gmail.com>
2019-01-21 21:18:40 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$config = [
+//"u=62050946;n=ffffffffb0f7a621ffffffffd14f73d1",
+"u=62016921;n=ffffffffbfb03e0a0000000060c83bad",//akun
+"u=62166484;n=ffffffffb0f7a621ffffffffd14f73d1",
+"u=62166010;n=ffffffff805b7ae38c742cda36142b17",
+"u=1145071;n=ffffffffb078f650ffffffff95596d31",
+"u=62026005;n=ffffffffb078f650ffffffff95596d31",
+"u=61562062;n=ffffffffb0f7a621ffffffffe8635462",
+"u=61619992;n=ffffffffb0f7a621ffffffffd14f73d1",//akun
+"u=61022486;n=ffffffffb0f7a621ffffffffd14f73d1",//akun
+"u=61620110;n=ffffffffb0f7a621ffffffffd14f73d1",//akun
+"u=61620174;n=ffffffffb0f7a621435c34170157142f",//akun
+"u=61723015;n=ffffffffb0f7a621ffffffffe8635462",//akun
+"u=61765065;n=ffffffffb0f7a621ffffffffe8635462",//akun
+"u=62779490;n=ffffffffb0f7a621ffffffffd14f73d1",//akun
+
+];
